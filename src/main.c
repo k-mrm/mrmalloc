4df6089b2f35e47e46ba79dc3208e6533ec7e7ce
mrm <kei.id0305@gmail.com>
2020-01-10 01:17:43 +0900

    add test and implement easy malloc

--- src/main.c
+++ src/main.c
@@ -2,8 +2,32 @@
 
 #include "malloc.h"
 
-int main(void) {
-    printf("Hello, malloc\n");
+typedef struct Person Person;
 
+struct Person {
+    char *name;
+    int age;
+    enum { MAN, WOMAN, OTHER } sex;
+};
+
+Person *new_person(char *name, int age, int sex) {
+    Person *new = mrmalloc(sizeof(Person));
+
+    new->name = name;
+    new->age = age;
+    new->sex = sex;
+}
+
+void dump_person(Person *p) {
+    printf("%s, %d, %s\n",
+           p->name,
+           p->age,
+           (p->sex == MAN) ? "man" : WOMAN ? "woman" : "other");
+}
+
+int main(void) {
+    Person *bob = new_person("iwashi", 371741, OTHER);
+
+    dump_person(bob);
     return 0;
 }
--- src/malloc.c
+++ src/malloc.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #include "malloc.h"
 
-/* k&r malloc */
-void *mrmalloc(size_t nbyte) {
-    return NULL;
+void *mrmalloc(size_t nbytes) {
+    if(nbytes == 0) {
+        return NULL;
+    }
+
+    void *ptr = sbrk(nbytes);
+    if(ptr == (void *)-1) {
+        return NULL;
+    }
+
+    return ptr;
 }
 
 void mrfree(void *ptr) {
-    ;
+    if(ptr == NULL) {
+        return;
+    }
 }
